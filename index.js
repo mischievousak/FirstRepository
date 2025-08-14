da8d592cd51ee59dbd11f9be795ec2a48a591e2b
Apurva Konde <apurvakonde345@gmail.com>
2025-08-14 14:03:52 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Your_Password',
+    database: '23be0033'
+});
+console.log('Connected to MYSQL Database!');
+
+connection.query(
+  'SELECT * FROM `instructor`',
+  function(err, results, fields) {
+    if (err) {
+      console.error('Error executing query:', err);
+      return;
+    }
+    console.log('Query results:', results); // results contains rows returned by server
+    console.log('Fields:', fields); // fields contains extra meta data about results
+  }
+);
+
+connection.end(err => {
+  if (err) {
+    console.error('Error closing connection:', err);
+    return;
+  }
+  console.log('Connection closed successfully.');
+
+});
